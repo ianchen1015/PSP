848d36b5009e3f0089bac677d11171979f87e367
ian <ianchen1015@gmail.com>
2018-11-26 17:23:53 +0800

    init

---
+++ generator.py
@@ -0,0 +1,42 @@
+import os
+import cv2
+import numpy as np
+
+from keras.preprocessing.image import img_to_array
+
+
+def category_label(labels, dims, n_labels):
+    x = np.zeros([dims[0], dims[1], n_labels])
+    for i in range(dims[0]):
+        for j in range(dims[1]):
+            x[i, j, labels[i][j]] = 1
+    x = x.reshape(dims[0] * dims[1], n_labels)
+
+    return x
+
+
+# generator that we will use to read the data from the directory
+def data_gen_small(img_dir, mask_dir, lists, batch_size, dims, n_labels):
+    while True:
+        ix = np.random.choice(np.arange(len(lists)), batch_size)
+        imgs = []
+        labels = []
+        for i in ix:
+            # images
+            img_path = img_dir + str(lists.iloc[i, 0]) + '.jpg'
+            #img_path = os.path.join(img_dir, lists.iloc[i, 0], '.jpg')
+            original_img = cv2.imread(img_path)[:, :, ::-1]
+            resized_img = cv2.resize(original_img, (dims[0], dims[1]))
+            array_img = img_to_array(resized_img) / 255
+            imgs.append(array_img)
+            # masks
+            mask_path = mask_dir + str(lists.iloc[i, 0]) + '.png'
+            #mask_path = os.path.join(img_dir, lists.iloc[i, 0], '.png')
+            original_mask = cv2.imread(mask_path)
+            resized_mask = cv2.resize(original_mask, (dims[0], dims[1]))
+            array_mask = category_label(resized_mask[:, :, 0], dims, n_labels)
+            labels.append(array_mask)
+        imgs = np.array(imgs)
+        labels = np.array(labels)
+
+        yield imgs, labels
---
+++ train.py
@@ -0,0 +1,136 @@
+import os
+import pandas as pd
+import argparse
+
+import keras.backend.tensorflow_backend as KTF
+import tensorflow as tf
+from keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
+
+from model import PSPNet50
+from generator import data_gen_small
+
+
+def argparer():
+    # command line argments
+    parser = argparse.ArgumentParser(description="PSPNet LIP dataset")
+    parser.add_argument("--train_list", help="train list path")
+    parser.add_argument("--trainimg_dir", help="train image dir path")
+    parser.add_argument("--trainmsk_dir", help="train mask dir path")
+    parser.add_argument("--val_list", help="val list path")
+    parser.add_argument("--valimg_dir", help="val image dir path")
+    parser.add_argument("--valmsk_dir", help="val mask dir path")
+    parser.add_argument("--batch_size", default=5, type=int,
+            help="batch size")
+    parser.add_argument("--n_epochs", default=10, type=int,
+            help="number of epoch")
+    parser.add_argument("--epoch_steps", default=6000, type=int,
+            help="number of epoch step")
+    parser.add_argument("--val_steps", default=1000, type=int,
+            help="number of valdation step")
+    parser.add_argument("--n_labels", default=20, type=int,
+            help="Number of label")
+    parser.add_argument("--input_shape", default=(512,512,3),#(512, 512, 3),
+            help="Input images shape")
+    parser.add_argument("--output_stride", default=16, type=int,
+            help="output stirde")
+    parser.add_argument("--output_mode", default="softmax", type=str,
+            help="output activation")
+    parser.add_argument("--upsample_type", default="deconv", type=str,
+            help="upsampling type")
+    parser.add_argument("--loss", default="categorical_crossentropy",
+            type=str, help="loss function")
+    parser.add_argument("--optimizer", default="adadelta", type=str,
+            help="oprimizer")
+    parser.add_argument("--gpu", default="0", type=str,
+            help="number of gpu")
+    parser.add_argument("--log_dir", default="./log/", type=str, help="log_dir")
+    args = parser.parse_args()
+
+    return args
+
+
+def main(args):
+    # device number
+    #if true:
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+
+    # set the necessary list
+    train_list = pd.read_csv(args.train_list, header=None)
+    val_list = pd.read_csv(args.val_list, header=None)
+
+    # set the necessary directories
+    trainimg_dir = args.trainimg_dir
+    trainmsk_dir = args.trainmsk_dir
+    valimg_dir = args.valimg_dir
+    valmsk_dir = args.valmsk_dir
+
+    # get old session old_session = KTF.get_session()
+
+    with tf.Graph().as_default():
+        session = tf.Session('')
+        KTF.set_session(session)
+        KTF.set_learning_phase(1)
+
+        # set callbacks
+        cp_cb = ModelCheckpoint(
+                filepath=args.log_dir,
+                monitor='val_loss',
+                verbose=1,
+                save_best_only=True,
+                mode='auto',
+                period=2)
+        es_cb = EarlyStopping(
+                monitor='val_loss',
+                patience=2,
+                verbose=1,
+                mode='auto')
+        tb_cb = TensorBoard(
+                log_dir=args.log_dir,
+                write_images=True)
+
+        # set generater
+        train_gen = data_gen_small(
+                trainimg_dir,
+                trainmsk_dir,
+                train_list,
+                args.batch_size,
+                [args.input_shape[0], args.input_shape[1]],
+                args.n_labels)
+        val_gen = data_gen_small(
+                valimg_dir,
+                valmsk_dir,
+                val_list,
+                args.batch_size,
+                [args.input_shape[0], args.input_shape[1]],
+                args.n_labels)
+
+        # set model
+        pspnet = PSPNet50(
+                input_shape=args.input_shape,
+                n_labels=args.n_labels,
+                output_mode=args.output_mode,
+                upsample_type=args.upsample_type)
+        print(pspnet.summary())
+
+        # compile model
+        pspnet.compile(
+                loss=args.loss,
+                optimizer=args.optimizer,
+                metrics=["accuracy"])
+
+        # fit with genarater
+        pspnet.fit_generator(
+                generator=train_gen,
+                steps_per_epoch=args.epoch_steps,
+                epochs=args.n_epochs,
+                validation_data=val_gen,
+                validation_steps=args.val_steps,
+                callbacks=[cp_cb, es_cb, tb_cb])
+        
+        pspnet.save_weights('./save/model.h5')
+        print('Model Saved')
+
+if __name__ == "__main__":
+
+    args = argparer()
+    main(args)
